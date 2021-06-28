7eec39497855053f47ec0154372b65bd94a20ef7
Roman Guseynov <hidagara@gmail.com>
2021-06-28 21:26:34 +0300

    ups example

---
+++ Whats new in SF Symbols/example_.swift
@@ -0,0 +1,20 @@
+//
+//  example.swift
+//  wwdc2021-collection
+//
+//  Created by Media Guest on 28.06.2021.
+//
+
+import Foundation
+import SwiftUI
+
+
+@available(iOS 15.0, *)
+struct Example {
+    func test() {
+        Canvas() { context, size in
+            let metrics = gridMetrics(in: size)
+        }
+    }
+    
+}
